6b13b907c0182b69e35e16b70106f389e6ccf500
teddy <teddylee777@gmail.com>
2024-06-17 20:14:37 +0900

    logging langsmith 업데이트

--- langchain_teddynote/logging.py
+++ langchain_teddynote/logging.py
@@ -2,7 +2,17 @@ import os
 
 
 def langsmith(project_name=None, set_enable=True):
+
     if set_enable:
+        result = os.environ.get("LANGCHAIN_API_KEY")
+        if result is None or result.strip() == "":
+            print(
+                "LangChain API Key가 설정되지 않았습니다. 참고: https://wikidocs.net/250954"
+            )
+            return
+        os.environ["LANGCHAIN_ENDPOINT"] = (
+            "https://api.smith.langchain.com"  # LangSmith API 엔드포인트
+        )
         os.environ["LANGCHAIN_TRACING_V2"] = "true"  # true: 활성화
         os.environ["LANGCHAIN_PROJECT"] = project_name  # 프로젝트명
         print(f"LangSmith 추적을 시작합니다.\n[프로젝트명]\n{project_name}")
